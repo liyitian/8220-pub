eada4429f841762876c2d096f88c6ed121c7b289
irvine <huang2chao1ran2@gmail.com>
2016-01-27 20:12:36 -0500

    test for ioctl

--- lab3/mymod.c
+++ lab3/mymod.c
@@ -1,255 +1,124 @@
 #include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/kernel_stat.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/pci.h>
-#include <linux/msi.h>
-#include <linux/mm.h>
-#include <linux/miscdevice.h>  
-#include <linux/delay.h>  
 #include <linux/kernel.h>  
 #include <linux/module.h>  
-#include <linux/init.h>  
 #include <linux/mm.h>  
 #include <linux/fs.h>  
-#include <linux/types.h>  
-#include <linux/delay.h>  
-#include <linux/moduleparam.h>  
-#include <linux/slab.h>  
-#include <linux/errno.h>  
 #include <linux/ioctl.h>  
 #include <linux/cdev.h>  
-#include <linux/string.h>  
-#include <linux/list.h>  
-#include <linux/pci.h>  
-#include <linux/gpio.h>
+#include <linux/pci.h>
 
-#include "reg.h"
-
-#define FIFO_ENTRIES 1024
-#define GRAPHICS_ON 1
-MODULE_LICENSE("Proprietary");
+MODULE_LICENSE("Proprietarty");
 MODULE_AUTHOR("Chaoran Huang");
 
-int fd=0;
-struct cdev kyouko3_cdev;
-
-struct fifo_entry{
-	u32 command;
-	u32 value;
-};
-
-struct fifo
-{
-	u64 p_base;
-	struct fifo_entry *k_base;
-	u32 head;
-	u32 tail_cache;
-};
-
-
 struct kyouko3_data{
-	unsigned int * p_control_base;
-	unsigned int * p_card_ram_base;
-	struct pci_dev * pdev;
-	unsigned int * k_control_base;
-	unsigned int * k_card_ram_base;
-//	void __iomem * ioaddr;
-	struct fifo fifo;
-	unsigned int graphics_on;
+    unsigned int* p_control_base;
+    unsigned int* p_card_ram_base;
+    unsigned int* k_control_base;
+    unsigned int* k_card_ram_base;
+    struct pci_dev *pdev;
 } kyouko3;
 
-struct pci_device_id kyouko3_dev_ids[]=
-{
-	{PCI_DEVICE(0x1234,0x1113)},
-	{0}
-};
 
-int kyouko3_probe(struct pci_dev * pci_dev, const struct pci_device_id * pci_id){
-	kyouko3.p_control_base=pci_resource_start(pci_dev,1);
-	kyouko3.p_card_ram_base=pci_resource_start(pci_dev,2);
-	kyouko3.pdev=pci_dev->irq;
-	pci_enable_device(pci_dev);
-	pci_set_master(pci_dev);
-};
-
-int kyouko3_remove(struct pci_dev * pci_dev){
-	pci_disable_device(pci_dev);
-}
-
-unsigned int K_READ_REG(unsigned int reg)
-{
-        unsigned int value;
-        //delay();
-        //rmb();
-        value = *(kyouko3.k_control_base+(reg>>2));
-        return (value);
-}
-
-void K_WRITE_REG(unsigned int reg, unsigned int value){
-	//delay();
-	*(kyouko3.k_control_base+(reg>>2))=value;
-}
-
-void pause(void){
-	while (K_READ_REG(FifoTail)!=0) 
-		schedule();//to some emergen guy then return;
-	//return (1);
-}
-
-int init_fifo(void){
-	kyouko3.fifo.k_base= pci_alloc_consistent(
-		kyouko3.pdev,
-		8192u,
-		&kyouko3.fifo.p_base
-		);
-	K_WRITE_REG(FifoStart,kyouko3.fifo.p_base);
-	K_WRITE_REG(FifoEnd,kyouko3.fifo.p_base+8192);
-	kyouko3.fifo.head=0;
-	kyouko3.fifo.tail_cache=0;
-	pause();
-}
-
-struct pci_driver kyouko3_pci_drv=
-{
-        .name="Kyouko3",
-        .id_table=kyouko3_dev_ids,
-        .probe=kyouko3_probe,
-        .remove=kyouko3_remove
-};
-
-
-void FIFO_WRITE(unsigned int reg, unsigned int value)
-{
-	kyouko3.fifo.k_base[kyouko3.fifo.head].command= reg;
-	kyouko3.fifo.k_base[kyouko3.fifo.head].value=value;
-	kyouko3.fifo.head++;
-	if(kyouko3.fifo.head >= FIFO_ENTRIES)
-	{
-		kyouko3.fifo.head=0;
-	}
+unsigned int K_READ_REG(unsigned int reg){
+    unsigned int  value;
+    //delay();
+    //rmb();
+    value = *(kyouko3.k_control_base+(reg>>2));
+    return (value);
 }
 
 
 int kyouko3_open(struct inode *inode, struct file *fp){
-	unsigned int ramsize;
-	kyouko3.k_control_base=ioremap(kyouko3.p_control_base,65536);
-	ramsize = K_READ_REG(Device_RAM);
-	ramsize *=(1024*1024);
-	kyouko3.k_card_ram_base=ioremap(kyouko3.p_card_ram_base,ramsize);
-	init_fifo();
-	printk(KERN_ALERT "Opened Kyouko3...");
-	return 0;
+    printk(KERN_ALERT "kyouko3_Open device");
+    kyouko3.k_control_base = ioremap(kyouko3.p_control_base, 65536);
+    unsigned int size = K_READ_REG(0x0020);
+    kyouko3.k_card_ram_base = ioremap(kyouko3.p_card_ram_base, size);
+    return 0;
 }
+
 
 int kyouko3_release(struct inode *inode, struct file *fp){
-
-	iounmap(kyouko3.k_control_base);
-	iounmap(kyouko3.k_card_ram_base);
-	pci_free_consistent(
-		kyouko3.pdev,
-		8192u,
-		kyouko3.fifo.k_base,
-		kyouko3.fifo.p_base
-		);
-	printk(KERN_ALERT "BUUH BYE...");
-	return 0;
-}
-int kyouko3_mmap(struct file *flip, struct vm_area_struct * vma){
-	int ret;
-	ret = remap_pfn_range(vma, vma->vm_start,(unsigned int )(kyouko3.p_control_base)>>PAGE_SHIFT, (unsigned long)(vma->vm_end-vma->vm_start), vma->vm_page_prot);
-	return ret;
-}
-
-kyouko3_ioctl(struct file *fp, unsigned int cmd, unsigned int arg)
-{
-	switch (cmd){
-		case VMODE:{
-			if (((int)(arg))==GRAPHICS_ON){
-				FIFO_WRITE(Frame_Objects+_FColumns,1024);
-				FIFO_WRITE(Frame_Objects+_FRows,768);
-				FIFO_WRITE(Frame_Objects+_FRowPitch,1024*4);
-				FIFO_WRITE(Frame_Objects+_FFormat,0xf888);
-				FIFO_WRITE(Frame_Objects+_FAddress,0);
-				//set acceleration bitmask to 0x40000000
-				FIFO_WRITE(Acceleration,0x40000000);
-				//set DAC
-				FIFO_WRITE(DAC_Objects+_DWidth,1024);
-				FIFO_WRITE(DAC_Objects+_DHeight,768);
-				FIFO_WRITE(DAC_Objects+_DVirtX,0);
-				FIFO_WRITE(DAC_Objects+_DVirtY,0);
-				FIFO_WRITE(DAC_Objects+_DFrame,0);
-				//set ModeSet
-				FIFO_WRITE(ModeSet,1);			
-				msleep(10);
-				float *RGBA=(float *)arg;
-				unsigned int uintR=*(unsigned int *) &RGBA[0];
-				unsigned int uintG=*(unsigned int *) &RGBA[1];
-				unsigned int uintB=*(unsigned int *) &RGBA[2];
-				unsigned int uintA=*(unsigned int *) &RGBA[3];
-				FIFO_WRITE(Clear_Color,uintR);
-				FIFO_WRITE(Clear_Color+4,uintG);
-				FIFO_WRITE(Clear_Color+8,uintB);
-				FIFO_WRITE(Clear_Color+12,uintA);
-				
-				FIFO_WRITE(Clear_Buffer,0x03);
-				FIFO_WRITE(Flush,0x0);
-				
-				kyouko3.graphics_on=1;
-			}
-			else{//off
-				FIFO_WRITE(Acceleration,0);
-				FIFO_WRITE(ModeSet,0);
-				kyouko3.graphics_on=0;
-			}			
-			break;
-		}
-		case FIFO_QUEUE:{
-			unsigned int ret;
-			struct fifo_entry entry;
-			ret=copy_from_user(
-				&entry, 
-				(struct fifo_entry *) arg,
-				sizeof(struct fifo_entry)
-				);
-			FIFO_WRITE(entry.command, entry.value);
-			break;
-		}
-		case FIFO_FLUSH:{
-			K_WRITE_REG(FifoHead,kyouko3.fifo.head);
-			while(kyouko3.fifo.tail_cache != kyouko3.fifo.head){
-				kyouko3.fifo.tail_cache= K_READ_REG(FifoTail);
-				schedule();
-			}
-			break;
-		}
-	}
+    iounmap(kyouko3.k_control_base);
+    iounmap(kyouko3.k_card_ram_base);
+    printk(KERN_ALERT "BUUH BYE");
+    //close(fd);
+    return 0;
 }
 
 
-struct file_operations kyouko3_fops = {
-	.open= 		kyouko3_open,
-	.release=	kyouko3_release,
-	.mmap=		kyouko3_mmap,
-	.owner=		THIS_MODULE
+int kyouko3_mmap(struct file *filp, struct vm_area_struct *vma){
+    int ret;
+    ret = remap_pfn_range(vma, vma->vm_start, (unsigned int)(kyouko3.p_control_base)>>PAGE_SHIFT, (unsigned long)(vma->vm_end-vma->vm_start), vma->vm_page_prot);
+    return ret;
+                                                              
+}
+
+int kyouko3_ioctl(){
+   int ret;
+   
+   return ret;
+}
+
+
+struct file_operations kyouko3_fops ={
+    .open = kyouko3_open,
+    .release = kyouko3_release,
+    .unlocked_ioctl = kyouko3_ioctl,
+    .mmap = kyouko3_mmap,
+    .owner = THIS_MODULE
 };
 
-int my_init_function(void)
-{
-	cdev_init(&kyouko3_cdev, &kyouko3_fops);
-	cdev_add(&kyouko3_cdev, MKDEV(500,127),1);
-	pci_register_driver(&kyouko3_pci_drv);
-	printk(KERN_ALERT "Initializing...");
-	return 0;
-}
-void my_exit_function(void)
-{	
-	cdev_del(&kyouko3_cdev);
-	pci_unregister_driver(&kyouko3_pci_drv);
-	printk(KERN_ALERT "Exiting...");
+
+struct cdev kyouko3_cdev;
+
+struct pci_device_id kyouko3_dev_ids[]={
+    {PCI_DEVICE(0x1234, 0x1113)},
+    {0},
+};
+
+
+int kyouko3_probe(struct pci_dev * pdev, const struct pci_dev_id * pciId){
+    
+    kyouko3.p_control_base = pci_resource_start(pdev, 1);
+    kyouko3.p_card_ram_base = pci_resource_start(pdev, 2);
+
+    pci_enable_device(pdev);
+
+    pci_set_master(pdev);
+
+    kyouko3.pdev = pdev;
 }
 
-module_init(my_init_function);
-module_exit(my_exit_function);
+void kyouko3_remove(struct pci_dev* pdev){
+    pci_disable_device(pdev);
+}
+
+
+struct pci_driver kyouko3_pci_drv = {
+    .name = "pci_driver",
+    .id_table = kyouko3_dev_ids,
+    .probe = kyouko3_probe,
+    .remove = kyouko3_remove
+};
+
+
+static int my_init_func(void)
+{
+    cdev_init(&kyouko3_cdev, &kyouko3_fops);
+    cdev_add(&kyouko3_cdev, MKDEV(500, 127), 1);
+    printk("init_module");
+    pci_register_driver(&kyouko3_pci_drv);
+    return 0;
+}
+
+static void my_exit_func(void)
+{
+    pci_unregister_driver(&kyouko3_pci_drv);
+    cdev_del(&kyouko3_cdev);
+    printk("exit_module");
+
+}
+
+module_init(my_init_func);
+module_exit(my_exit_func);
+
--- lab3/user.c
+++ lab3/user.c
@@ -3,70 +3,27 @@
 #include <errno.h>
 #include <math.h>
 #include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-
-#define GRAPHICS_ON 1
-
-#define VMODE                   _IOW (0xcc,0,unsigned long)
-#define BIND_DMA                _IOW (0xcc,1,unsigned long)
-#define UNBIND_DMA              _IOW (0xcc,5,unsigned long)
-#define START_DMA               _IOWR(0xcc,2,unsigned long)
-#define FIFO_QUEUE              _IOWR(0xcc,3,unsigned long)
-#define FIFO_FLUSH              _IO  (0xcc,4)
-//#define VMODE 0
-//#define FIFO_QUEUE 3
-//#define FIFO_FLUSH 4
 
 struct u_kyouko_device{
    unsigned int *u_control_base;
-   unsigned int *u_framebuffer_base;
 } kyouko3;
 
 #define KYOUKO_CONTROL_SIZE (65535)
 #define Device_RAM (0x0020)
 
-
-struct fifo_entry{
-   unsigned int command;
-   unsigned int value;
-};
-
 unsigned int U_READ_REG(unsigned int rgister){
    return (*(kyouko3.u_control_base+(rgister>>2)));
-}
-
-
-void U_WRITE_FB(unsigned int address, unsigned int value){
-   *(kyouko3.u_framebuffer_base + address) = value;
 }
 
 int main()
 {
    int fd;
    int result;
-   unsigned int i;
-   
+
    fd = open("/dev/kyouko3", O_RDWR);
-
-   kyouko3.u_control_base = mmap(0, KYOUKO_CONTROL_SIZE, PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-
-   kyouko3.u_framebuffer_base = mmap(0x80000000, KYOUKO_CONTROL_SIZE, PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-
-   ioctl(fd, VMODE, GRAPHICS_ON);
-   
-   for (i = 200*1024; i < 201*1024; ++i){
-      U_WRITE_FB(i, 0xff0000);
-   }
-
-   struct fifo_entry entry;
-   entry.command = 0x5010;
-   entry.value = 0;
-   ioctl(fd, FIFO_QUEUE, &entry);
-   ioctl(fd, FIFO_FLUSH, GRAPHICS_ON);
-   sleep();
-
-   ioctl(fd, VMODE, 0);
+   kyouko3.u_control_base = (unsigned int *)mmap(0, KYOUKO_CONTROL_SIZE, PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+   result = U_READ_REG(Device_RAM);
+   printf("Ram size in MB is: %d\n", result);
    close(fd);
    return 0;
 }
