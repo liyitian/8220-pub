1ee0c076807e6ce5ef27f6b0a8eb03eb1acd6484
YaolongYu <yaolony@g.clemson.edu>
2016-03-31 18:24:51 -0400

    a

--- lab8/smunch.c
+++ lab8/smunch.c
@@ -8,20 +8,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<signal.h>
+#include<unistd.h>
+#include<sys/types.h>
 #define smunch(pid, bit_pattern) syscall(331, pid, bit_pattern) 
+
+void sighandler(int arg)
+{
+	printf("sighandler arg:%d\n", arg);
+}
 
 int main(int argc, char *argv[])
 {
-    sigset_t s;
-	sigemptyset(&s);
-	sigaddset(&s, SIGKILL);
+    sigset_t sigs;
 	int ret;
+	ret = sigemptyset(&sigs);
+	sigaddset(&sigs, SIGINT);
+	//ret = sigaddset(&sigs, SIGKILL);
+	//sigfillset(&sigs);
 	
 	if(argc<2){
 		printf("need pid!\n");
 		return 1;
 	}
-
+	printf("ret: %d sigs:%x\n", ret ,sigs);
+	
 	ret = smunch(atoi(argv[1]), 256);
 	printf("smunch return: %d\n", ret);
 	return 0;
